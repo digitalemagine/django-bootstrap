0259d5249eb080207619685f490294040171b8be
Stefano Crosta <stefano.crosta@gmail.com>
2013-01-17 01:09:47 +0100

    include_package_data=True

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author=u'Stefano Crosta',
     author_email='stefano@digitalemagine.com',    
     packages=find_packages(),
+    include_package_data=True
     url='http://github.com:digitalemagine/django-bootstrap.git',    
     license='Open Source',
     description='Simple integration of bootstrap by twitter into django',
